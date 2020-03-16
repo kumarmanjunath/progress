5fc557ccb5c6ff8b3e1ee089dcf8ed03abff875a
kumarmanjunth <kumarmanjunath197@gmai.com>
2020-03-16 09:34:28 +0530

    Signup added

---
+++ src/productList.js
@@ -0,0 +1,71 @@
+const productList = [
+  {
+    id: 1,
+    productName: "Dr.Rita",
+    imgUrl:
+      "https://images.unsplash.com/photo-1579165466949-3180a3d056d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    about: "Retired Doctor in Victoria Hospital",
+    expierence: "25 Years"
+  },
+  {
+    id: 2,
+    productName: "Dilip Singh",
+    imgUrl:
+      "https://images.unsplash.com/photo-1546946974-e8769324eeab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    about: "Retired Army Officer",
+    expierence: "20 Years"
+  },
+  {
+    id: 3,
+    productName: "Martin Raff",
+    imgUrl:
+      "",
+    expierence: "15 Years"
+  },
+  {
+    id: 4,
+    productName: "Mrs.Barb",
+    imgUrl:
+      "https://images.unsplash.com/photo-1573497490850-f4178bb9e133?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    about: "Retired IT Employee",
+    expierence: "20 Years"
+  },
+  {
+    id: 5,
+    productName: "Michal Jordan",
+    imgUrl:
+      "https://images.unsplash.com/flagged/photo-1557896279-080cb03b9ca6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
+    about: "Retired Lecturer",
+    expierence: "30 years "
+  },
+  {
+    id: 6,
+    productName: "Mrs.Anita",
+    imgUrl:
+      "https://images.unsplash.com/flagged/photo-1579911301495-8b7e49c78f4d?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    about: "Retired Banking Employee",
+    expierence: "20 Years"
+  },
+  {
+    id: 7,
+    productName: "Mrs.Camille",
+    imgUrl:
+      "https://images.unsplash.com/photo-1551180452-aea351b23949?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    about: "Artist",
+    expierence: "20 Years"
+  },
+  {
+    id: 8,
+    productName: "Jhonothan Root",
+    imgUrl:
+      "https://images.unsplash.com/photo-1547363484-c07b94906964?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    about: "Photographer",
+    expierence: "15 Years"
+  }
+];
+
+export default productList;
+
+
+https://images.unsplash.com/photo-1576670158386-6fb29d77b2ba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=383&q=80",
+about: "Retired German Scientist
