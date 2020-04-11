809a2082dcdd8f41fd99abd37cc0b353779768af
kumarmanjunth <kumarmanjunath197@gmai.com>
2020-04-11 22:09:59 +0530

    Added my project

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const config = require("config");
 
-module.exports = function(req, res, next) {
+module.exports = function (req, res, next) {
   // Get token from header
   const token = req.header("x-auth-token");
 
@@ -18,4 +18,17 @@ module.exports = function(req, res, next) {
   } catch (err) {
     res.status(401).json({ msg: "Token is not valid" });
   }
+};
+exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorResponse(
+          `User role ${req.user.role} is not authorized to access this route`,
+          403
+        )
+      );
+    }
+    next();
+  };
 };
--- models/User.js
+++ models/User.js
@@ -3,21 +3,26 @@ const mongoose = require("mongoose");
 const UserSchema = mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
+  },
+  role: {
+    type: String,
+    enum: ["user", "mentors"],
+    default: "user",
   },
   password: {
     type: String,
-    required: true
+    required: true,
   },
   date: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model("user", UserSchema);
